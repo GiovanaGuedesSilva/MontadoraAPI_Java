42c169d8bbd40fb8959757ba6f1e7b41d96b9fbf
GiovanaGuedesSilva <giovanagwynguedes@gmail.com>
2025-06-22 18:23:49 -0300

    finalziando modulo de criacao de anottations

--- src/main/java/io/github/giovanaguedessilva/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ src/main/java/io/github/giovanaguedessilva/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -9,13 +9,35 @@ import org.springframework.context.annotation.Configuration;
 public class MontadoraConfiguration {
 
     @Bean
-    public Motor motor (){
+    public Motor motorAspirado (){
         var motor = new Motor();
         motor.setCavalos(120);
         motor.setTipoMotor(TipoMotor.ASPIRADO);
         motor.setCilindros(12);
         motor.setLitragem(2.0);
         motor.setModelo("XPTO-0");
+        return motor;
+    }
+
+    @Bean
+    public Motor motorEletrico (){
+        var motor = new Motor();
+        motor.setCavalos(100);
+        motor.setTipoMotor(TipoMotor.ELETRICO);
+        motor.setCilindros(1);
+        motor.setLitragem(3.0);
+        motor.setModelo("TH-40");
+        return motor;
+    }
+
+    @Bean
+    public Motor motorTurbo (){
+        var motor = new Motor();
+        motor.setCavalos(190);
+        motor.setTipoMotor(TipoMotor.TURBO);
+        motor.setCilindros(13);
+        motor.setLitragem(4.0);
+        motor.setModelo("XX-5H");
         return motor;
     }
 }
